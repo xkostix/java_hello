77d832d3cb42b26444d01ed19f6ad88492b3de5b
xkostix <mkostik@yandex.ru>
2023-11-25 19:01:45 +0700

    Add task1.java

---
+++ Seminar01/task1/src/task1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        Scanner scan = new Scanner(System.in);
+    }
+}
